c594cedb2ac4273d7d9d12edc0fd85410d7e8b3a
Prakash Kandel <unique_prakash2002@yahoo.com>
2018-04-04 11:36:45 +1000

    Add base and attribute model

---
+++ backend/api/models/attributes.model.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+const TechEnum = ['Front End', 'Back End', 'Mobile IOS', 'Mobile Android', 'Dev Ops', 'Database', 'Other'];
+const attributeSchema = new Schema ({
+    title: { type: String, required: true},
+    description: {type: String, required: true},
+    category:{ type: enum, enum: TechEnum },
+    public: { type: Boolean, required: true, default: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    base: { type: Schema.Types.ObjectId, ref: 'BaseEntity'},
+    created_on: { type: Date, required: true, default: new Date()},
+    modified_on: { type:Date, required: true },
+
+});
+
+export default mongoose.model('Attribute', attributeSchema);
--- backend/api/models/base_entity.model.js
+++ backend/api/models/base_entity.model.js
@@ -3,9 +3,11 @@ const Schema = mongoose.Schema;
 
 const baseEntitySchema = new Schema ({
     title: { type: String, required: true},
+    description: String,
     user: { type: Schema.Types.ObjectId, ref: 'User' },
     created_on: { type: Date, required: true, default: new Date()},
-    modified_on: { type:Date, required: true }
+    modified_on: { type:Date, required: true },
+    public: { type: Boolean, required: true, default: true }
 });
 
 export default mongoose.model('BaseEntity', baseEntitySchema);
