4b5bf940d430803947140587ff46b479d92d03b1
Prakash Kandel <unique_prakash2002@yahoo.com>
2018-04-04 11:38:33 +1000

    Modify attribute model to accept note / blog attribute

--- backend/api/models/attributes.model.js
+++ backend/api/models/attributes.model.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 const TechEnum = ['Front End', 'Back End', 'Mobile IOS', 'Mobile Android', 'Dev Ops', 'Database', 'Other'];
+const noteEnum = ['note', 'blog', 'both'];
 const attributeSchema = new Schema ({
     title: { type: String, required: true},
     description: {type: String, required: true},
     category:{ type: enum, enum: TechEnum },
     public: { type: Boolean, required: true, default: true },
+    type: { type: enum, enum: noteEnum, required: true },
     user: { type: Schema.Types.ObjectId, ref: 'User' },
     base: { type: Schema.Types.ObjectId, ref: 'BaseEntity'},
     created_on: { type: Date, required: true, default: new Date()},
