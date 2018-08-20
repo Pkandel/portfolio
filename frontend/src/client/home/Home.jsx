6bdf6639fa2fdcb29130853d0a8054dba5aa283b
Prakash Kandel <unique_prakash2002@yahoo.com>
2018-08-20 21:53:52 +1000

    Added markdown and highlightjs

--- frontend/src/client/home/Home.jsx
+++ frontend/src/client/home/Home.jsx
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import hljs from 'highlight.js';
+import './home.scss';
+
+const showdown = require('showdown'),
+	converter = new showdown.Converter();
 
 class Home extends Component {
-
+	constructor() {
+		super();
+		this.state = {
+			markdown: '',
+			html: converter.makeHtml('# Hello World')
+		};
+	}
+	componentDidMount() {
+		hljs.initHighlightingOnLoad();
+	}
+	handleChange = (e) => {
+		this.setState({
+			markdown: e.target.value,
+			html: converter.makeHtml(e.target.value)
+		}, () => {
+			const code = document.querySelectorAll('code');
+			if (code.length) {
+				code.forEach(c => {
+					hljs.highlightBlock(c);
+				});
+			}
+		}
+		);
+	}
 	render() {
-    	return (
-    		<div>
-    		<h1> This is client Home </h1>
-    		</div>
-    	);
+		return (
+			<div className="home-component">
+				<textarea
+					name="markdown"
+					value={this.state.markdown}
+					onChange={this.handleChange}
+				/>
+				<h1> This is client Home </h1>
+				<div dangerouslySetInnerHTML={{ __html: `${this.state.html}`}} />
+			</div>
+		);
 	}
 }
 export default Home;
