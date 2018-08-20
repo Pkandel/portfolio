e9764d1a7e69c7eca7302b25087adf731358b783
Prakash Kandel <unique_prakash2002@yahoo.com>
2018-08-20 23:38:53 +1000

    Added some style for table and blockquote and default markdown option as github

--- frontend/src/client/home/Home.jsx
+++ frontend/src/client/home/Home.jsx
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import hljs from 'highlight.js';
 import './home.scss';
 
-const showdown = require('showdown'),
-	converter = new showdown.Converter();
+const showdown = require('showdown');
 
 class Home extends Component {
 	constructor() {
 		super();
+		// see below for individual setup
+		showdown.setFlavor('github');
+		this.converter = new showdown.Converter();
 		this.state = {
 			markdown: '',
-			html: converter.makeHtml('# Hello World')
+			html: this.converter.makeHtml('# Hello World')
 		};
 	}
 	componentDidMount() {
@@ -19,7 +21,7 @@ class Home extends Component {
 	handleChange = (e) => {
 		this.setState({
 			markdown: e.target.value,
-			html: converter.makeHtml(e.target.value)
+			html: this.converter.makeHtml(e.target.value)
 		}, () => {
 			const code = document.querySelectorAll('code');
 			if (code.length) {
@@ -45,3 +47,37 @@ class Home extends Component {
 	}
 }
 export default Home;
+// const converterOptions = {
+// 	strikethrough: true,
+// 	tables: true,
+// 	omitExtraWLInCodeBlocks: true,
+// 	parseImgDimensions: true,
+// 	simplifiedAutoLink: true,
+// 	excludeTrailingPunctuationFromURLs: true,
+// 	tasklists: true,
+// 	smoothLivePreview: true,
+// 	smartIndentationFix: true,
+// 	disableForced4SpacesIndentedSublists: true,
+// 	simpleLineBreaks: true,
+// 	ghMentions: true,
+// 	backslashEscapesHTMLTags:true,
+// 	emoji: true
+
+// };
+// this.converter = new showdown.Converter(converterOptions);
+
+/* parseImage dimentions
+![foo](foo.jpg =100x80)     simple, assumes units are in px
+![bar](bar.jpg =100x*)      sets the height to "auto"
+![baz](baz.jpg =80%x5em)  Image with width of 80% and height of 5em
+*/
+/* simplyfiedAutoLink
+some text www.google.com becomes
+<p>some text <a href="www.google.com">www.google.com</a>
+*/
+/* excludeTrailingPunctuationFromURLs
+some text www.google.com! becomes
+<p>check this link <a href="www.google.com">www.google.com</a>!</p>
+*/
+
+// for emoji :smile: list here https://github.com/showdownjs/showdown/wiki/Emojis
