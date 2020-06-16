2195395752efd928d798c985f3ad61389e03e8d6
Prakash Kandel <prakashk@outlook.com.au>
2020-06-16 21:22:53 +1000

    Fix duplicate icons issue

--- frontend/src/Root.jsx
+++ frontend/src/Root.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Layout from './Layout.jsx';
+import './style.scss';
 import { hot } from 'react-hot-loader';
 
 class Root extends Component {
